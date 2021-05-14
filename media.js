f7d8819f578835ff2c51635d8e46481346b58c23
km nurunnabi <km.nurunnabi66@gmail.com>
2021-05-14 19:11:34 +0600

    Update media.js

--- media.js
+++ media.js
@@ -1,6 +1,24 @@
 /*
 =========================================================================================================================================
-simply copy and paste this code into 
+simply copy and paste this code into your function.php file. 
+
+But remember one important thing. Don't forget to enqueue the wp_enqueue_media() function in function.php file & admin_enqueue_scripts
+hook, given below for example::
+
+<?php 
+
+ function sunset_load_admin_scripts() {
+
+    wp_enqueue_media(  );
+
+    wp_register_script( 'sunset-admin-script', get_template_directory_uri() . '/js/sunset.admin.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script('sunset-admin-script');
+
+ }
+ add_action( 'admin_enqueue_scripts', 'sunset_load_admin_scripts' );
+ 
+ ?>
+ 
 =========================================================================================================================================
 */
 
